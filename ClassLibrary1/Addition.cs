57987de66dd7a8c79c492a9e1c86678b9f58ac46
Mukumbi <NSAMBAM@HQ-MCS-ICTM72>
2020-01-09 11:37:20 +0200

    Add project files.

---
+++ ClassLibrary1/Addition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Addition
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public static double sum(double[] arrayA)
+
+        {
+            double c = 0.0;
+            foreach (double a in arrayA)
+            {
+                c = sum(a, c);
+            }
+            return c;
+        }
+    }
+}
